a202927affe9d3f19093baa196184bc8f134f74b
Egor <egor.starodumov2012@yandex.ru>
2024-05-06 18:18:18 +0700

    Обновление App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Это мобильное приложение YOOKING!</Text>
       <StatusBar style="auto" />
     </View>
   );
